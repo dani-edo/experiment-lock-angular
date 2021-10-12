ff88fdee45469fc3cb477b11776d985d80eb0e9d
Dani Edo <dani.nur.afandi@gmail.com>
2021-10-12 18:10:27 +0700

    set angular 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MomentDateTimeAdapter } from 'ng-pick-datetime-moment';
+import { DateTimeAdapter, OWL_DATE_TIME_LOCALE } from 'ng-pick-datetime';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: DateTimeAdapter,
+      useClass: MomentDateTimeAdapter,
+      deps: [OWL_DATE_TIME_LOCALE],
+    },
+  ],
 })
 export class AppComponent {
   title = 'experiment-lock';
